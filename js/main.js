f8cc24158a401e7dbe9c2f9f14302b20a402e16a
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-03-29 17:24:12 -0700

    add working logic and include jquery

--- js/main.js
+++ js/main.js
@@ -1,51 +1,94 @@
+// $(function(){
 console.log('Tic Tac Toe script loaded');
 
 // Variables
+var nextMove, currentPlayer, state, winner, gameOverMsg;
 
-var x = [1, 'X'];
+var $displayEl = $('#display');
+var $playSpace = $('table');
 
-var o = [-1, 'O'];
-
-var nextMove = [x, o, x, o, x, o, x, o, x, o];
-var currentPlayer = nextMove[0];
-
-var displayEl = document.getElementById('display');
-var playSpace = document.querySelector('table');
+var winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 //Event Listeners
 
-playSpace.addEventListener('click', runTurn);
+$playSpace.on('click', 'td', runTurn);
 
-document.getElementById('reset').addEventListener('click', resetFunc);
+$('#reset').on('click', resetFunc);
 
 // Functions
 
+
 function runTurn(evt) {
-  if (evt.target.id === 'display' || evt.target.id === 'reset') return;
-  // add symbol of nextMove[0] to the evt.target.textContent
-  if (currentPlayer === x){
-    evt.target.textContent = "❌";
-  } else evt.target.textContent = "⭕️";
-  // add the value of nextMove[0] to the square (1/-1)
-  evt.target.value = currentPlayer[0];
-  // check to see if a winning move has occured - sum of one of 8 paths = 3 or -3
-  winFunc();
-  // make that cell unselectable now
-  // update who's up on the display nextmove.shift();
+  var id = this.id;
+  if (state[id]) return;
+  state[id] = currentPlayer === 'x' ? 1 : -1;
+  // turn *= -1;
+  winner = getWinner();
+  if (winner) {
+    gameOverMsg = 'Congrats player ' + (winner === 1 ? 'X' : 'O') + '!';
+  } else if (!state.includes(0)) {
+    gameOverMsg = "Rats, another cat's game";
+  }
   nextMove.shift();
   currentPlayer = nextMove[0];
-  displayEl.textContent = "Player " + currentPlayer[1] + ": You're up!";
+
+  render();
 }
 
-function winFunc() {
-
+function resetFunc() {
+  nextMove = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x', 'o'];
+  currentPlayer = nextMove[0];
+  state = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  winner = 0;
+  gameOverMsg = '';
+  render();
 }
 
-function resetFunc () {
-  nextMove = [x, o, x, o, x, o, x, o, x, o];
-  displayEl.textContent = "Select a square to make the first move";
-  document.querySelectorAll('td').textContent = ""; //not working
+function render() {
+  // set cell with id's content to whatever the element is in the state array at the same index
+  state.forEach(function(val,i) {
+    var $sq = $('#' + i);
+    if(val) {
+      $sq.html(val === 1 ? 'X' : 'O');
+    } else {
+      $sq.html('');
+    }
+  });
+  if (gameOverMsg) {
+    $displayEl.html(gameOverMsg);
+  } else {
+    $displayEl.html('Player ' + currentPlayer + ': You\'re up!');
+  }
 }
+
+//Win Logic
+function sumThree(combo){
+  return state[combo[0]] + state[combo[1]] + state[combo[2]];
+}
+
+// iterates the winning combos through the state array and sumss them, returns 1 or -1 for winner
+function getWinner(){
+  for (var i=0; i<winningCombos.length; i++) {
+    var sum = sumThree(winningCombos[i]);
+    if (sum === 3 || sum === -3) {
+      return (sum / 3);
+    }
+  }
+}
+
+
 
 // Initialize
-console.log(resetFunc());
+resetFunc();
+
+
+// });
